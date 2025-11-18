54166e70e18fd06689699f0aa706e998794302dc
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2025-11-18 13:11:46 +0530

    Commit

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,14 +1,27 @@
 
 class Human{
 private String name="Allmeen"; // when you declare the variable as private now it restricts the user and they wont be able to access in the other class 
-private int age= 24; // therefore if you want to access then you can create a method 
+private int age= 24; // therefore if you want to access then you can create a method called getter and setter
 
+// Getters
+// get method is utilized to get the data from the other class
 public int getAge(){
     return age;
 }
 
 public String getName(){
     return name;
+}
+
+// Setters 
+// set method is utlized to set the data for that particular and specific variable
+
+public void setAge(int a){
+    age=a;
+}
+
+public void setName(String n){
+    name =n;
 }
 
 }
@@ -23,6 +36,12 @@ public class Encapsulation {
      //System.out.println(obj.name);// error because the variable is private and cannot be accesed directly 
     
      System.out.println(obj.getName()+ " : " +obj.getAge());// now using the method you can access the variable 
+      
 
+     obj.setAge(25);
+     obj.setName("Arshi");
+     System.out.println(obj.getAge());
+
+    
     }
 }
---
+++ this_keyword.java
@@ -0,0 +1,37 @@
+
+class Car{
+private double speed;
+private String color;
+
+/*
+ public void set_Speed(double speed){ // here the variable speed and s are different however what if i keep the s variable as speed then the local variable and the instance/global variable will remain same then it creates a confusion in assigning the value
+    speed=speed;  // this will print 0.0 
+}
+ */
+
+
+// this keyword
+// to differentiate the instance variable and local variable this keyword is used 
+// this keyword refers to the current object which calls the method here it is obj
+// obj refers to the Car class object thus it refers and signals it to the instance variable of the class
+
+public void set_Speed(double speed){ 
+      this. speed=speed;  // now it will assign to the instance/global variable
+}
+public double get_speed(){
+    return speed;
+}
+
+public String get_color(){
+    return color;
+}
+}
+
+
+public class this_keyword {
+    public static void main(String [] args){
+    Car obj= new Car();
+    obj.set_Speed(200);
+    System.out.println(obj.get_speed());
+    }
+}

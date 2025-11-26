fdee3a2addb74ad7175533b85d4e8ac09cbdaad8
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2025-11-26 22:03:20 +0530

    Commit

---
+++ Object_Class.java
@@ -0,0 +1,17 @@
+// You know that every class in java extends the Object class
+// when you try to print the Object of the class it will print at hexadecimal code/hash code
+// 
+
+class Laptop{
+
+}
+
+
+
+public class Object_Class {
+    public static void main(String[] args){
+ 
+        Laptop obj = new Laptop();
+        System.out.println(obj); // whenever you print the obj by default it is obj.toString
+    }
+}
